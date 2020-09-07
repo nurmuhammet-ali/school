5cce6febb819afe751b799c7f5c69772e31da111
Sanish Gurung <sanish@drishya.com.np>
2020-09-07 12:38:27 +0545

    validation fixes

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -9,6 +9,7 @@ use App\Student;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use Illuminate\Validation\Rule;
 
 class StudentController extends Controller
 {
@@ -51,7 +52,13 @@ class StudentController extends Controller
             'password'          => 'required|string|min:8',
             'parent_id'         => 'required|numeric',
             'class_id'          => 'required|numeric',
-            'roll_number'       => 'required|numeric|unique:students',
+            'roll_number'       => [
+                'required',
+                'numeric',
+                Rule::unique('students')->where(function ($query) use ($request) {
+                    return $query->where('class_id', $request->class_id);
+                })
+            ],
             'gender'            => 'required|string',
             'phone'             => 'required|string|max:255',
             'dateofbirth'       => 'required|date',
@@ -132,7 +139,13 @@ class StudentController extends Controller
             'email'             => 'required|string|email|max:255|unique:users,email,'.$student->user_id,
             'parent_id'         => 'required|numeric',
             'class_id'          => 'required|numeric',
-            'roll_number'       => 'required|numeric|unique:students,roll_number,'.$student->id,
+            'roll_number'       => [
+                'required',
+                'numeric',
+                Rule::unique('students')->ignore($student->id)->where(function ($query) use ($request) {
+                    return $query->where('class_id', $request->class_id);
+                })
+            ],
             'gender'            => 'required|string',
             'phone'             => 'required|string|max:255',
             'dateofbirth'       => 'required|date',
